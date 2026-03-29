d426c5a097d9d5b622d81117f9273c3a54b317dd
Julian V <j.vilfort01@gmail.com>
2026-03-29 02:47:23 -0400

    Modify deploy file

--- deploy.example.sh
+++ deploy.example.sh
@@ -7,23 +7,24 @@ set -e
 
 #----------------- CONFIGURABLES -----------------
 REMOTE_USER_HOST="[netid]@coewww.rutgers.edu"  # Replace "[netid]" with your netid
-REMOTE_DIR="/www/www6/star"      
-BUILD_DIR="./dist" # Name of the output directory after building react app
+REMOTE_DIR="/www/www6/star"
+BUILD_DIR="./dist"    # Name of the output directory after building the react app
+DEPLOY_NAME="react-app"             # Name of the folder on the remote server
 #----------------- END OF CONFIGURABLES -----------------
 
 # Check that build directory exists
 if [ ! -d "$BUILD_DIR" ]; then
-  echo "Error: 'build' directory not found in current working directory."
+  echo "Error: '${BUILD_DIR}' directory not found in current working directory."
   exit 1
 fi
 
-echo "Copying build directory to remote..."
+# Get just the folder name (e.g. "dist" from "./dist")
+DIR_NAME=$(basename "$BUILD_DIR")
+
+echo "Copying ${DIR_NAME} to remote..."
 scp -r "$BUILD_DIR" "$REMOTE_USER_HOST:$REMOTE_DIR/"
 
-echo "Removing old react-app on remote..."
-ssh "$REMOTE_USER_HOST" "rm -rf $REMOTE_DIR/react-app"
+echo "Removing old ${DEPLOY_NAME} and renaming ${DIR_NAME} to ${DEPLOY_NAME} on remote..."
+ssh "$REMOTE_USER_HOST" "rm -rf $REMOTE_DIR/$DEPLOY_NAME && mv $REMOTE_DIR/$DIR_NAME $REMOTE_DIR/$DEPLOY_NAME"
 
-echo "Renaming build to react-app on remote..."
-ssh "$REMOTE_USER_HOST" "mv $REMOTE_DIR/build $REMOTE_DIR/react-app"
-
-echo "Done! Build deployed to $REMOTE_DIR/react-app"
+echo "Done! Build deployed to $REMOTE_DIR/$DEPLOY_NAME"
